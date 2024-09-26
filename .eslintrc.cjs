24f9c7fc96eef58645b2639d4b6357f63d0a7377
fanfarexo <fanfarexo@gmail.com>
2024-09-26 09:05:09 +0900

    chore: eslint 설정 확장 및 타입 체크 강화

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,26 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
+    'eslint:recommended', // 기본 ESLint 규칙
+    'plugin:@typescript-eslint/recommended-type-checked', // TypeScript 기본 규칙
+    'plugin:@typescript-eslint/stylistic-type-checked', // TypeScript 스타일 규칙
+    'plugin:react/recommended', // React 기본 규칙
+    'plugin:react-hooks/recommended', // React Hooks 규칙
+    'plugin:react/jsx-runtime', // React 18+ JSX runtime 설정
+    'plugin:prettier/recommended', // Prettier 규칙 적용
   ],
+  settings: {
+    react: {
+      version: 'detect', // React 버전 자동 감지
+    },
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', '@typescript-eslint'],
