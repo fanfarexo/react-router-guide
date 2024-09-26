9c9739e224cd8b0b36b1f8f9c1dd8e4b9bc9aac1
fanfarexo <fanfarexo@gmail.com>
2024-09-26 10:55:01 +0900

    feat: URI 파라미터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,18 @@ import About from './quickstart-router-basics/pages/About';
 import Members from './quickstart-router-basics/pages/Members';
 import SongList from './quickstart-router-basics/pages/SongList';
 import { useState } from 'react';
+import SongDetail from './quickstart-router-basics/pages/SongDetail';
 
 export type MemberType = {
   name: string;
   photo: string;
+};
+
+export type SongType = {
+  id: number;
+  title: string;
+  musician: string;
+  youtube_link: string;
 };
 
 const App = () => {
@@ -19,6 +27,22 @@ const App = () => {
     { name: 'Scott King', photo: 'photos/King.png' },
     { name: 'Johnny Pike', photo: 'photos/JPike.jpg' },
     { name: 'Toby Ruckert', photo: 'photos/Toby.jpg' },
+  ]);
+
+  const [songs] = useState<SongType[]>([
+    { id: 1, title: "Fallin' for you", musician: 'Colbie callet', youtube_link: 'PABUl_EX_hw' },
+    { id: 2, title: "Can't hurry love", musician: 'The supremes', youtube_link: 'EJDPhjQft04' },
+    { id: 3, title: 'Landslide', musician: 'Dixie chicks', youtube_link: 'V2N7gYom9-A' },
+    { id: 4, title: "Can't let go", musician: 'Linda ronstadt', youtube_link: 'P-EpGKXmoe4' },
+    { id: 5, title: 'Doctor my eyes', musician: 'Jackson Browne', youtube_link: '7JlFKS_1oZk' },
+    {
+      id: 6,
+      title: 'We gotta get you a woman',
+      musician: 'Todd Rundgren',
+      youtube_link: 'EyUjbBViAGE',
+    },
+    { id: 7, title: 'Hip to my heart', musician: 'Band Perry', youtube_link: 'vpLCFnD9LFo' },
+    { id: 8, title: 'Rolling in the deep', musician: 'Adele', youtube_link: 'EvK8pDK6IQU' },
   ]);
 
   return (
@@ -35,8 +59,11 @@ const App = () => {
           <Route path='/members' element={<Members members={members} />}>
             Members
           </Route>
-          <Route path='/songs' element={<SongList />}>
+          <Route path='/songs' element={<SongList songs={songs} />}>
             SongList
+          </Route>
+          <Route path='/songs/:id' element={<SongDetail songs={songs} />}>
+            SongDetail
           </Route>
         </Routes>
       </div>
--- src/quickstart-router-basics/pages/SongList.tsx
+++ src/quickstart-router-basics/pages/SongList.tsx
@@ -1,7 +1,25 @@
-const SongList = () => {
+import { Link } from 'react-router-dom';
+import { SongType } from '../../App';
+
+type Props = {
+  songs: SongType[];
+};
+
+const SongList = (props: Props) => {
+  const list = props.songs.map((song) => {
+    return (
+      <li className='list-group-item' key={song.id}>
+        <Link to={`/songs/${song.id}`} style={{ textDecoration: 'none' }}>
+          {song.title} ( {song.musician} )
+        </Link>
+      </li>
+    );
+  });
+
   return (
     <div className='card card-body'>
-      <h2>SongList</h2>
+      <h2 className='mt-4 mb-2'>SongList</h2>
+      <ul className='list-group'>{list}</ul>
     </div>
   );
 };
