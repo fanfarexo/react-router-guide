471b40c30edaebf1a855acbc41dd852c5c8cf08c
fanfarexo <fanfarexo@gmail.com>
2024-09-26 13:45:55 +0900

    feat: 리액트 라우터 훅, useSearchParams

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,5 +27,11 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        varsIgnorePattern: '^_',
+      },
+    ],
   },
 };
