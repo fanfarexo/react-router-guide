99c56466b1f607db59c17fc130a2d5f3f5ff69a8
fanfarexo <fanfarexo@gmail.com>
2024-09-26 10:10:25 +0900

    feat: 라우팅된 컴포넌트의 객체 속성 전달

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,23 @@ import Home from './quickstart-router-basics/pages/Home';
 import About from './quickstart-router-basics/pages/About';
 import Members from './quickstart-router-basics/pages/Members';
 import SongList from './quickstart-router-basics/pages/SongList';
+import { useState } from 'react';
+
+export type MemberType = {
+  name: string;
+  photo: string;
+};
 
 const App = () => {
+  const [members] = useState<MemberType[]>([
+    { name: 'Maggie Adams', photo: 'photos/Mag.png' },
+    { name: 'Sammie Purcell', photo: 'photos/Sam.png' },
+    { name: 'Tim Purcell', photo: 'photos/Tim.png' },
+    { name: 'Scott King', photo: 'photos/King.png' },
+    { name: 'Johnny Pike', photo: 'photos/JPike.jpg' },
+    { name: 'Toby Ruckert', photo: 'photos/Toby.jpg' },
+  ]);
+
   return (
     <Router>
       <div className='container'>
@@ -17,7 +32,7 @@ const App = () => {
           <Route path='/about' element={<About title={'여우와 늙다리들'} />}>
             About
           </Route>
-          <Route path='/members' element={<Members />}>
+          <Route path='/members' element={<Members members={members} />}>
             Members
           </Route>
           <Route path='/songs' element={<SongList />}>
--- src/quickstart-router-basics/pages/Members.tsx
+++ src/quickstart-router-basics/pages/Members.tsx
@@ -1,7 +1,27 @@
-const Members = () => {
+import { MemberType } from '../../App';
+
+type Props = { members: MemberType[] };
+
+const Members = (props: Props) => {
+  const imgstyle = { width: 90, height: 80 };
+  const list = props.members.map((member) => {
+    return (
+      <div key={member.name} className='col-6 col-md-4 col-lg-3'>
+        <img src={member.photo} alt={member.name} className='img-thumbnail' style={imgstyle} />
+        <br />
+        <h6>{member.name}</h6>
+        <br />
+        <br />
+      </div>
+    );
+  });
+
   return (
-    <div className='card card-body'>
-      <h2>Members</h2>
+    <div>
+      <h2 className='m-4'>Members</h2>
+      <div className='container'>
+        <div className='row'>{list}</div>
+      </div>
     </div>
   );
 };
