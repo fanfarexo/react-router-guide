78928bf6828b07d09c77a12a134dd805bfe7e166
fanfarexo <fanfarexo@gmail.com>
2024-09-26 09:52:36 +0900

    feat: 기초 컴포넌트 추가 및 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './quickstart-router-basics/components/Header';
+import Home from './quickstart-router-basics/pages/Home';
+import About from './quickstart-router-basics/pages/About';
+import Members from './quickstart-router-basics/pages/Members';
+import SongList from './quickstart-router-basics/pages/SongList';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <div className='container'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}>
+            Home
+          </Route>
+          <Route path='/about' element={<About />}>
+            About
+          </Route>
+          <Route path='/members' element={<Members />}>
+            Members
+          </Route>
+          <Route path='/songs' element={<SongList />}>
+            SongList
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
